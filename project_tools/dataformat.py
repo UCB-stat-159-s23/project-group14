e3bcfcb09a50a083cbe2862fe8bab1c3cfd1d623
Daniel <79027463+hjang8659@users.noreply.github.com>
2023-05-05 16:48:23 -0700

    added basic funs

--- project_tools/dataformat.py
+++ project_tools/dataformat.py
@@ -1,8 +1,43 @@
-from ligotools import readligo as rl
+import warnings
+warnings.filterwarnings('ignore')
+
 import pandas as pd
+import numpy as np
 
 
-def import_data():
+def import_data(df):
+    """
+    
+    Input: Stored path variable
+    
+    Return: Dataframe
+    
+    """
+    
+    print("Generating dataframe")
     return pd.read_csv('data/states_sales.csv')
 
 
+
+def save_data(df):
+    """
+    
+    Save the data
+    
+    """
+    print("Saving data")
+    return data.to_csv(df index=True)
+
+
+
+def df_len(df):
+    """
+    
+    Input: Dataframe
+    
+    Return: Size (row and column) of the dataframe
+    
+    
+    """
+    print("Printing its size")
+    print("Row(s): ", df.shape[0], "Column(s): ", df.shape[0])
