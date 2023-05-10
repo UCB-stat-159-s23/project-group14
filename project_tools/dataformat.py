7af6a287c3b00ccddc6ecec438e43952d84e2301
Zilin Zhang <zilinz@berkeley.edu>
2023-05-10 07:15:35 +0000

    add docstrings and managed _toc.yml:

--- project_tools/dataformat.py
+++ project_tools/dataformat.py
@@ -7,26 +7,41 @@ import numpy as np
 warnings.filterwarnings('ignore')
 
 def tobacco_data_import():
+    '''
+    This function process the tobacco data that we downloaded from U.S.
+    Chronic Disease Indicators (CDI).
+    It reads the U.S._Chronic file _Disease_Indicators__Tobacco.csv file
+    and returns a preferrable data frame that we will use to plot graphs
+    and to analyze.
+    '''
     data = pd.read_csv('data/U.S._Chronic_Disease_Indicators__Tobacco.csv', low_memory=False)
+
     # filter question
     data = data[data["Question"] == "Sale of cigarette packs"]
-    #fulter columns
+
+    # filter columns
     data = data[['YearStart', 'LocationAbbr', 'LocationDesc', 'Question', 'DataValueUnit', 'DataValue']]
     data = data.groupby(['YearStart', 'LocationDesc']).sum().reset_index()
+
     # remove states GU, PR, US, VI since they are not actual states.
     data = data.loc[data['DataValue'] != 0.0]
     data.to_csv("data/states_sales.csv")
+
     return data
 
 
 def income_data_import():
     '''
-    This function 
+    This function process the median income data that we downloaded from U.S.
+    Bureau of the Census, Current Population Survey, Annual Social and
+    Economic Supplements.
+    It reads in the median_income.csv file and returns a preferrable data frame
+    that we will use to plot graphs and to analyze.
     '''
     median_DATA = pd.read_csv('data/median_income.csv')
     cols_drop = median_DATA.columns[median_DATA.columns.str.contains("Standard")]
     median_income = median_DATA.drop(columns=cols_drop).set_index("Location")
-    #drop 2017(40) and 2013(39)
+    # drop 2017(40) and 2013(39)
     median_income = median_income.drop(median_income.columns[[0, 1, 4, 9]], axis = 1)
     us_state_to_abbrev = {
         "Alabama": "AL",
@@ -87,10 +102,11 @@ def income_data_import():
     median_income.iloc[:, 1:len(median_income)] = median_income.iloc[:, 1:len(median_income)].replace(',','', regex=True).astype(float)
     median_income["avg_med_income"] = median_income.iloc[:, 1:len(median_income)].mean(axis = 1)
 
-    #Abbreviate the States for plotly geographical mapping
+    # Abbreviate the States for plotly geographical mapping
     median_income["state"] =  median_income["Location"]
     median_income = median_income.replace({"state": us_state_to_abbrev}).drop(0)
 
     # median_income.columns.values[7] = '2013 Median income'
     median_income = median_income.rename(columns={median_income.columns.tolist()[7]:'2013 Median income'})
+
     return median_income
